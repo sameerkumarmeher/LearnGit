b76d2da37622fe9f9903370d05c0b78780852d26
sameerkumarmeher <sameermeher005@gmail.com>
2023-08-01 10:20:13 +0530

    practice git

---
+++ A.java
@@ -0,0 +1,10 @@
+public class A
+{
+    public static void main(String[] args) throws Exception {
+        for (int i=0;i<10;i++) {
+            System.out.println("hello"); 
+            Thread.sleep(1000);
+        }
+           
+    }
+}
